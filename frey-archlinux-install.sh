1fa109fbd81fcd665ef06e1b76bef173d9846460
FreyNell <freybalaguera@gmail.com>
2023-01-24 00:47:39 -0500

    update

--- frey-archlinux-install.sh
+++ frey-archlinux-install.sh
@@ -14,62 +14,70 @@ mount $ARCHPART /mnt
 mount --mkdir $EFIPART /mnt/boot
 swapon $SWAPPART
 
-
 pacman -Sy
-pacstrap -K /mnt base linux linux-firmware
-pacstrap -K /mnt xorg xorg-xinit xorg-apps xorg-server nvidia nvidia-utils networkmanager networkmanager-openvpn vi vim
-genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap -K /mnt base linux linux-firmware \
+xorg xorg-xinit xorg-apps xorg-server \
+nvidia nvidia-utils \
+networkmanager networkmanager-openvpn \
+vi vim \
+grub efibootmgros-prober amd-ucode \
+zsh sudo bitwarden-cli reflector htop bspwm sxhkd \
+polybar xdg-user-dirs firefox openssh git acpi \
+libxkbcommon libxkbcommon-x11 wget
 
+genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot
 
-
 ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
 hwclock --systohc
+
 sed -i 's/#es_CO/es_CO/' /etc/locale.gen
+
 locale-gen
+
 echo "LANG=es_CO.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=la-latin1" >> /etc/vconsole.conf
 echo "ARCHFREY" >> /etc/hostname
 
 echo "Please create your ROOT user password"
 passwd
-pacman -S grub efibootmgr
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=$GRUB_ENTRY
-pacman -S os-prober
 
 sed -i 's/#GRUB_DISABLE_OS_PROBE/GRUB_DISABLE_OS_PROBE/' /etc/default/grub
-pacman -S amd-ucode
+
 grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S zsh
-useradd -m -G freynell -s /bin/zsh freynell
+
+useradd -m -s /bin/zsh freynell
+
 echo "Add password for freynell"
 passwd freynell
-pacman -S sudo
-pacman -S bitwarden-cli
+
 sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-pacman -S reflector
-pacman -S htop
-pacman -S bspwm sxhkd
 
-pacman -S polybar
-pacman -S xdg-user-dirs
-pacman -S firefox
-pacman -S openssh
-pacman -S git
-pacman -S acpi
+su freynell
+cd ~
+xdg-user-dirs-update
+mkdir -p /home/freynell/.config/bspwm
+mkdir -p /home/freynell/.config/sxhkd
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc /home/freynell/.config/bspwm/bspwmrc
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc /home/freynell/.config/sxhkd/sxhkdrc
 
-sudo -u freynell xdg-user-dirs-update
-sudo -u freynell mkdir -p /home/freynell/.config/bspwm
-sudo -u freynell mkdir -p /home/freynell/.config/sxhkd
-sudo -u freynell install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc /home/freynell/.config/bspwm/bspwmrc
-sudo -u freynell install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc /home/freynell/.config/sxhkd/sxhkdrc
-
-pacman -S libxkbcommon libxkbcommon-x11
 localectl set-x11-keymap latam
 
 echo "Edit /home/freynell/.xinitrc"
+wget https://raw.githubusercontent.com/FreyNell/frey-archlinux/main/.xinitrc
+
 echo "Edit sxhkdrc configuration"
-echo "Use startx bspwm to start bspwm"
-sudo -u freynell sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://raw.githubusercontent.com/FreyNell/frey-archlinux/main/.config/sxhkd/sxhkdrc -O /home/freynell/.config/sxhkd/sxhkdrc
+
+echo "Edit bspwmrc"
+wget https://raw.githubusercontent.com/FreyNell/frey-archlinux/main/.config/bspwm/bspwmrc -O /home/freynell/.config/bspwm/bspwmrc
+
+echo "Zprofile file is for run kitty and bspwm as soon as I log in."
+wget https://raw.githubusercontent.com/FreyNell/frey-archlinux/main/.zprofile
+
+echo "Install Oh My Zsh!"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
