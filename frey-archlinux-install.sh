406181bab764025903b84aa1e55935663b300e24
freynell <freybalaguera@gmail.com>
2023-01-27 01:18:58 -0500

    added sec tools

--- frey-archlinux-install.sh
+++ frey-archlinux-install.sh
@@ -27,7 +27,7 @@ neovim zsh sudo bitwarden-cli reflector htop xdg-user-dirs \
 bspwm sxhkd polybar firefox openssh git wget dmenu autorandr \
 mesa-utils vi kitty imagemagick pulseaudio pulseaudio-alsa \
 xbindkeys ufw gufw hdparm ranger feh \
-lynis clamav clamtk
+lynis clamav clamtk rkhunter
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
